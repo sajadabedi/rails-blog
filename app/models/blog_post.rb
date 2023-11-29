565b5d34166aecf5e285b98d2490baacc30ffcf6
Sajjad Abedi <sajadxabedi@gmail.com>
2023-11-29 22:28:39 +0200

    installed action_text

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -2,7 +2,12 @@ class BlogPost < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
 
-  scope :sorted, -> { order(published_at: :desc, updated_at: :desc) }
+  scope :sorted,
+        -> do
+          order(arel_table[:published_at].desc.nulls_last).order(
+            updated_at: :desc
+          )
+        end
   scope :draft, -> { where(published_at: nil) }
   scope :published, -> { where("published_at <= ?", Time.current) }
   scope :scheduled, -> { where("published_at > ?", Time.current) }

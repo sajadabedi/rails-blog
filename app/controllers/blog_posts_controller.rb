30ab6d11e35d943bbb074ede2ddd207f24ba93c0
Sajjad Abedi <sajadxabedi@gmail.com>
2023-11-29 14:33:13 +0200

    Refactor blog post sorting and display

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -3,7 +3,7 @@ class BlogPostsController < ApplicationController
   before_action :set_blog_post, except: %i[index new create]
 
   def index
-    @blog_posts = user_signed_in? ? BlogPost.all : BlogPost.published
+    @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
   end
 
   def show
